17d70bac4811ff6f519f15aa5bee897530fd9997
rogerzhu <rogerzhu0710@126.com>
2018-11-09 21:01:55 +0800

    adjust the code and provide try catch for stablize

--- myGAND.py
+++ myGAND.py
@@ -7,23 +7,50 @@ from bs4 import BeautifulSoup
 import json
 import time
 
+def GetBSObject(url,headers,encoding=None):
+    bsObj = None
+    try:
+        request = Request(url=url,headers=headers)
+        html = urlopen(request)
+        if encoding is not None:
+            bsObj = BeautifulSoup(html,fromEncoding=encoding)
+        else:
+            bsObj = BeautifulSoup(html)           
+    except error.HTTPError as e:
+        print("Error in Getting BSObjetc.")
+    finally:
+        return bsObj
+
+def GetJSON(jsonStr):
+    dataJson = ""
+    try:
+        strJson = jsonStr[jsonStr.find("{"):jsonStr.rfind("}")+1]
+        dataJson = json.loads(strJson)
+        #print(dataJson)
+    except json.decoder.JSONDecodeError as e:  
+        pass
+    finally:
+        return dataJson
+
+
+
+
+
 url = "https://list.jd.com/list.html?cat=1713,3287,3797"
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64)'
            'AppleWebKit/537.36 (KHTML, like Gecko)'
            ' Chrome/55.0.2883.103 Safari/537.36'}
-request = Request(url=url,headers=headers)
-html = urlopen(request)
-bsObj = BeautifulSoup(html)
+
+bsObj = GetBSObject(url,headers)
 allItemInfo = bsObj.findAll("li",{"class":"gl-item"})
-#print(allItemInfo[0])
 
 dictItem = {}
 nTestCount = 2
-while nTestCount <= 3:
-    #dictItemValue = {}
+while nTestCount <= 50:
     for ItemInfo in allItemInfo:
-        #dictItemValue.clear()
         dictItemValue = {}
+        
+        # Get dataSku,name,title and url
         dataSkuA = ItemInfo.find("a",{"class":"p-o-btn focus J_focus"})
         dataSku = dataSkuA["data-sku"]
         pName = ItemInfo.find("div",{"class":"p-name"})
@@ -32,49 +59,48 @@ while nTestCount <= 3:
         
         # Get Price
         priceURL = "https://p.3.cn/prices/mgets?callback=jQuery1&skuIds=J_"+dataSku
-        request = Request(url=priceURL,headers=headers)
-        html = urlopen(request)
-        bsObj=BeautifulSoup(html)
-        strReturn = str(bsObj)
-        strJson = strReturn[strReturn.find("{"):strReturn.rfind("}")+1]
-        dataJson = json.loads(strJson)
-        
+        bsObj=GetBSObject(priceURL,headers)
+        dataJson = GetJSON(str(bsObj))
+        price = 0
+        try:
+            price = dataJson["p"]
+        except TypeError as e:
+            price = -1
+
         # Get the goodRate
         pageSize = 1
         urlVeryDetailsJson = "https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv4374&productId="+dataSku+"&score=0&sortType=5&page=0&pageSize="+str(pageSize)+"&isShadowSku=0&fold=1"
-        request = Request(url=urlVeryDetailsJson,headers=headers)
-        html = urlopen(request)
-        bsObj = BeautifulSoup(html,fromEncoding="gb18030")
-        strReturnDetails = str(bsObj)
-        strReturnDetails = strReturnDetails[strReturnDetails.find("{"):strReturnDetails.rfind("}")+1]  
+        bsObj = GetBSObject(urlVeryDetailsJson,headers,"gb18030")
+        jsonPageComments = GetJSON(str(bsObj))
         goodRate = 0
         try:
-            jsonPageComments = json.loads(strReturnDetails)
-            #print(jsonPageComments)
             goodRate = jsonPageComments["productCommentSummary"]["goodRate"]
-        except json.decoder.JSONDecodeError as e:            
-            goodRate = 0       
+        except TypeError as e:
+            goodRate = -1
 
-        # add into a Dict.
+        # Add into a Dict.
         dictItemValue["title"] = title
         dictItemValue["itemURL"] = itemURL
-        dictItemValue["price"] = dataJson["p"]
+        dictItemValue["price"] = price
         dictItemValue["goodRate"] = int(goodRate*100)
-        #dictItem[dataSku] = title+","+itemURL+","+dataJson["p"]
         dictItem[dataSku] = dictItemValue
+        
+        # Print for interactive
+        strValueToPrint = (title,itemURL,price,str(goodRate*100))
+        print(",".join(strValueToPrint))
 
+    time.sleep(3)
     # move to next page
     nextPageURL ="https://list.jd.com/list.html?cat=1713,3287,3797&page="+str(nTestCount)+"&sort=sort_rank_asc&trans=1&JL=6_0_0" 
-    request = Request(url=nextPageURL,headers=headers)
-    html = urlopen(request)
-    bsObj = BeautifulSoup(html)
+    bsObj = GetBSObject(nextPageURL,headers)
     allItemInfo.clear()
     allItemInfo = bsObj.findAll("li",{"class":"gl-item"})
+
     nTestCount += 1
 
-
+'''
 for key in dictItem:
     for subKey in dictItem[key]:
         #pass
         print(subKey+":"+str(dictItem[key][subKey]))
-
+'''
