67eda9dde6a4bcd874af1ade2943ce92cb78907f
rogerzhu <rogerzhu0710@126.com>
2018-11-12 22:08:57 +0800

    Not a simple but also not so useful log

--- myGAND.py
+++ myGAND.py
@@ -7,80 +7,94 @@ from bs4 import BeautifulSoup
 import json
 import time
 import logging
+import functools
 
 
-logging.basicConfig(format='[%(asctime)s] %(message)s',level=logging.INFO)
+def InitialLog():
+    logger = logging.getLogger("funny logger")
+    logger.setLevel(logging.INFO)
 
-def SimpleLog(func):
-    def wrapper(*args,**kwargs):
-        logging.info("Enter into :%s" % func.__name__)
-        func(*args,**kwargs)
-        logging.info("Exit :%s" % func.__name__)
-        return func(*args,**kwargs)
-    return wrapper
+    fh = logging.FileHandler("./funny_log.log")
+    format="[%(asctime)s] %(name)s:%(message)s"
+    formatter = logging.Formatter(format)
+    fh.setFormatter(formatter)
 
-@SimpleLog
+    logger.addHandler(fh)
+    return logger
+
+
+def NotSoSimpleLog(bPass):
+    def ActualDecorator(func):
+        @functools.wraps(func)
+        def Wrapper(*args,**kwargs):
+            logger = InitialLog()
+            logger.info("Enter into :%s" % func.__name__)
+            try:
+                func(*args,**kwargs)
+            except:
+                err = "exception raised in"+func.__name__
+                logger.exception(err)
+                if bPass:
+                    raise
+                else:
+                    pass
+            finally:
+                logger.info("Exit :%s" % func.__name__)
+            return func(*args,**kwargs)
+        return Wrapper
+    return ActualDecorator
+
+@NotSoSimpleLog(True)
 def GetBSObject(url,headers,encoding=None):
     bsObj = None
-    try:
-        request = Request(url=url,headers=headers)
-        html = urlopen(request)
-        if encoding is not None:
-            bsObj = BeautifulSoup(html,fromEncoding=encoding)
-        else:
-            bsObj = BeautifulSoup(html)
-    except error.HTTPError as e:
-        print("Error in Getting BSObjetc.")
-    finally:
-        return bsObj
+    request = Request(url=url,headers=headers)
+    html = urlopen(request)
+    if encoding is not None:
+        bsObj = BeautifulSoup(html,fromEncoding=encoding)
+    else:
+        bsObj = BeautifulSoup(html)
+    return bsObj
 
+
+@NotSoSimpleLog(False)
 def GetJSON(jsonStr):
     dataJson = ""
-    try:
-        strJson = jsonStr[jsonStr.find("{"):jsonStr.rfind("}")+1]
-        dataJson = json.loads(strJson)
-        #print(dataJson)
-    except json.decoder.JSONDecodeError as e:  
-        pass
-    finally:
-        return dataJson
+    strJson = jsonStr[jsonStr.find("{"):jsonStr.rfind("}")+1]
+    dataJson = json.loads(strJson)
+    return dataJson
 
-
+@NotSoSimpleLog(False)
 def GetDataSKU(ItemInfo):
     dataSkuA = ItemInfo.find("a",{"class":"p-o-btn focus J_focus"})
     dataSku = dataSkuA["data-sku"]
     return dataSku
 
+@NotSoSimpleLog(False)
 def GetTitleAndItemUrl(ItemInfo):
     pName = ItemInfo.find("div",{"class":"p-name"})
     title = pName.em.get_text()
     itemURL = "https:"+pName.a["href"]
     return title,itemURL
 
+@NotSoSimpleLog(False)
 def GetPrice(ItemInfo,dataSku):
     priceURL = "https://p.3.cn/prices/mgets?callback=jQuery1&skuIds=J_"+dataSku
     bsObj=GetBSObject(priceURL,headers)
     dataJson = GetJSON(str(bsObj))
     price = 0
-    try:
-        price = dataJson["p"]
-    except TypeError as e:
-        price = -1
-    finally:
-        return price
+    price = dataJson["p"]
+    return price
 
+@NotSoSimpleLog(False)
 def GetGoodRate(ItemInfo,dataSku,pageSize):
         urlVeryDetailsJson = "https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv4374&productId="+dataSku+"&score=0&sortType=5&page=0&pageSize="+str(pageSize)+"&isShadowSku=0&fold=1"
         bsObj = GetBSObject(urlVeryDetailsJson,headers,"gb18030")
         jsonPageComments = GetJSON(str(bsObj))
         goodRate = 0
-        try:
-            goodRate = jsonPageComments["productCommentSummary"]["goodRate"]
-        except TypeError as e:
-            goodRate = -1
-        finally:
-            return goodRate
+        goodRate = jsonPageComments["productCommentSummary"]["goodRate"]
+        return goodRate
 
+@NotSoSimpleLog(True)
 def MoveToNextPage(nPageIndex):
     nextPageURL ="https://list.jd.com/list.html?cat=1713,3287,3797&page="+str(nPageIndex)+"&sort=sort_rank_asc&trans=1&JL=6_0_0" 
     bsObj = GetBSObject(nextPageURL,headers)
@@ -122,7 +136,7 @@ while nTestCount <= 3:
         dictItem[dataSku] = dictItemValue
         
         # Print for interactive
-        strValueToPrint = (title,itemURL,price,str(goodRate*100))
+        strValueToPrint = (dataSku,title,itemURL,price,str(goodRate*100))
         print(",".join(strValueToPrint))
 
     time.sleep(3)
